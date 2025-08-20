f64b5b29eee6abbbc433b41ef8d9a8c82bae1d3d
Death007 <mohanborle17@gmail.com>
2025-08-20 23:30:07 +0530

    updates

--- src/api/projects.jsx
+++ src/api/projects.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE = "https://iusspuber5.execute-api.eu-north-1.amazonaws.com/default/fetchProjects";
+const API_BASE = "https://gtyzk9m0vb.execute-api.eu-north-1.amazonaws.com/dev/fetchprojects";
 
 export const getProjects = async () => {
-  const res = await axios.get(`${API_BASE}/projects`);
+  const res = await axios.get(`${API_BASE}`);
   return res.data;
 };
