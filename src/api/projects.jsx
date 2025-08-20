cf4e6ac83b20dcb0d7ae33c63178dfd65fb82f95
Death007 <mohanborle17@gmail.com>
2025-08-20 23:11:53 +0530

    updated the API Url in ReactJS

--- src/api/projects.jsx
+++ src/api/projects.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://iusspuber5.execute-api.eu-north-1.amazonaws.com/default/fetchProjects";
 
 export const getProjects = async () => {
   const res = await axios.get(`${API_BASE}/projects`);
